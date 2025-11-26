f5459f4121793ef5114f90806e7bc2a393945e7b
Gigacore <santhoshinbang@gmail.com>
2025-11-26 08:39:46 +0530

    github link fix

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => (
           &copy; {new Date().getFullYear()} TripMeter
         </p>
         <a
-          href="https://github.com/sansunda/trip-visualizer"
+          href="https://github.com/Gigacore/TripMeter"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white transition-colors order-1 sm:order-2"
